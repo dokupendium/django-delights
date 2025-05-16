a9e9e1db757bd1bfb94d4f19c8ef3913a2bde715
dokupendium <mimic@dokupendium.de>
2025-05-16 02:24:49 +0200

    Ändere den Import der Bestandsverwaltungs-App auf die neue App-Konfiguration

--- djangodelights/settings.py
+++ djangodelights/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangodelights.inventory',  # Custom app for inventory management
+    'inventory.apps.InventoryConfig',
 ]
 
 MIDDLEWARE = [
